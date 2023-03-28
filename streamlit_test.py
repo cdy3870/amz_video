447a8a202df1a65eeeb3a89c60171b99df3351ec
cdy3870 <thefallen3870@yahoo.com>
2023-03-28 18:38:12 -0400

    updated streamlit

--- streamlit_test.py
+++ streamlit_test.py
@@ -10,8 +10,8 @@ import seaborn as sns
 
 st.set_page_config(layout="wide")
 st.title("Analysis of Amazon Video")
-with urlopen("https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson") as response:
-    countries = json.load(response)
+# st.runtime.legacy_caching.clear_cache()
+
 prof_countries = ["India", "United States", "United Kingdom"]
 
 def get_data():
@@ -22,10 +22,12 @@ def get_data():
 	return data
 
 
-def get_geoplot_fig(data, type="Primary"):
+def get_geoplot_fig(data, type="High"):
+	with urlopen("https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson") as response:
+	    countries = json.load(response)
 	movies_per_coun = data["single_country"].value_counts()
 	df = pd.DataFrame({"country":movies_per_coun.index, "counts":movies_per_coun.values})
-	if type == "Primary":
+	if type == "High":
 		df = df[df["country"].isin(prof_countries)]
 		df["country"] = df["country"].apply(lambda x : x.replace("United States", "United States of America"))
 		max_range = 300
@@ -43,13 +45,13 @@ def get_geoplot_fig(data, type="Primary"):
 	                           mapbox_style="carto-positron",
 	                           zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
 	                           opacity=0.5,
-	                           labels={'counts':'TV Shows/Movies in Country'}
+	                           labels={'counts':'TV Shows/Movies'}
 	                          )
 	fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
 	return fig
 
-def get_heatmap_fig(data, type="Primary"):
+def get_heatmap_fig(data, type="High"):
 	data["listed_in"] = data["listed_in"].apply(lambda x : x.replace(" ", "")
                                                         .replace("andCulture", "Culture")
                                                         .split(","))
@@ -70,14 +72,14 @@ def get_heatmap_fig(data, type="Primary"):
 
 	fig = plt.figure(figsize=(10,5))
 
-	if type == "Primary":
-		plt.title("Genre Counts in Primary Production Country")
+	if type == "High":
+		plt.title("Genre Counts in High Production Country")
 		plt.xticks(rotation=90)
 		sns.heatmap(genre_counts_df[genre_counts_df["single_country"].isin(prof_countries)]
 		                            .iloc[:, 1:].to_numpy(), vmin = 0, vmax = 200, cmap="Greens", yticklabels=prof_countries, xticklabels=new_col_names)
 
 	else:
-		plt.title("Genre Counts in Secondary Production Country")
+		plt.title("Genre Counts in Low Production Country")
 		plt.xticks(rotation=90)
 		temp_countries = prof_countries + ["Unknown"]
 		other_countries = [c for c in countries if c not in temp_countries]
@@ -85,6 +87,39 @@ def get_heatmap_fig(data, type="Primary"):
 		                            .iloc[:, 1:].to_numpy(), vmin = 0, vmax = 15, cmap="Blues", yticklabels=other_countries, xticklabels=new_col_names)
 
 	return fig
+
+@st.cache_data
+def cache_get_stacked_primary(data):
+	temp = data.groupby(["single_country", "type"]).size().unstack(fill_value=0).reset_index()
+	temp = temp[temp["single_country"].isin(prof_countries)]
+	fig = px.bar(temp, x="single_country", y=["Movie", "TV Show"], title="TV/Movies per Country")
+	
+	return fig
+
+@st.cache_data
+def cache_get_stacked_secondary(data):
+	temp = data.groupby(["single_country", "type"]).size().unstack(fill_value=0).reset_index()
+	temp = temp[~temp["single_country"].isin(prof_countries + ["Unknown"])]
+	fig = px.bar(temp, x="single_country", y=["Movie", "TV Show"], title="TV/Movies per Country")
+	
+	return fig
+
+
+@st.cache_data
+def cache_get_heatmap_fig_primary(data, country_type="High"):
+	heatmap_fig = get_heatmap_fig(data, country_type)
+	return heatmap_fig
+
+@st.cache_data
+def cache_get_heatmap_fig_secondary(data, country_type="Low"):
+	heatmap_fig = get_heatmap_fig(data, country_type)
+	return heatmap_fig
+
+
+@st.cache_data
+def cache_get_geoplot_fig(data, country_type):
+	geo_fig = get_geoplot_fig(data, country_type)
+	return geo_fig
 
 def main():
 	data = get_data()
@@ -94,18 +129,29 @@ def main():
 	metric_col1.markdown('##### Author: Calvin Yu\n')
 	st.markdown("""---""")	
 
-	# st.sidebar.markdown('### Primary or Secondary Countries')
-	country_type = st.radio("Primary or Secondary Countries", ("Primary", "Secondary"))
-	heatmap_fig = get_heatmap_fig(data, country_type)
-	geo_fig = get_geoplot_fig(data, country_type)
+	
+	with st.sidebar:
+		country_type = st.radio("High or Low Production Countries", ("Low", "High"))
+		min_counts = st.slider('Minimum totals', 0, 1000)
+
+	if country_type == "High":
+		heatmap_fig = cache_get_heatmap_fig_primary(data)
+		stacked_fig = cache_get_stacked_primary(data)
+	elif country_type == "Low":
+		heatmap_fig = cache_get_heatmap_fig_secondary(data)
+		stacked_fig = cache_get_stacked_secondary(data)
+
+	geo_fig = cache_get_geoplot_fig(data, country_type)
 
 	st.plotly_chart(geo_fig, use_container_width=True)
-	st.pyplot(heatmap_fig)
+	chart1, chart2 = st.columns(2)
+	chart1.plotly_chart(stacked_fig, use_container_width=True)
+	chart2.pyplot(heatmap_fig)
 
 	show_df = st.checkbox("Show raw data")
 
 	if show_df:
-		st.dataframe(data.head(), use_container_width=True)
+		st.dataframe(data.head())
 
 
 
